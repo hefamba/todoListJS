d44f991017ebc3e38b4d309a13927b5589ae1e79
Henry Efamba jr <henry@Henrys-MacBook-Pro.local>
2023-08-30 13:38:46 -0500

    done ready for review

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const form = document.getElementById('form')
 const input = document.getElementById('input')
 const todosUl = document.getElementById('todos')
 
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+if (todos){
+    todos.forEach(todo => addToDo(todo))
+}
+
 form.addEventListener('submit', (event)=>{
     event.preventDefault()
 
@@ -25,16 +31,28 @@ function addToDo(todo){
 
        todoEl.addEventListener('click' , ()=>{
         todoEl.classList.toggle('completed')
+        updateLS()
        })
 
        todoEl.addEventListener('contextmenu', (event) =>{
         event.preventDefault()
         todoEl.remove()
+        updateLS()
        })
     }
-
-
     input.value = ''
+    updateLS()
+}
 
+function updateLS(){
+    const todosEl = document.querySelectorAll('li')
 
+    const todosArr = []
+
+    todosEl.forEach(todo => todosArr.push({
+        text: todo.innerText,
+        completed: todo.classList.contains('completed') 
+    }))
+
+    localStorage.setItem('todos', JSON.stringify(todosArr))
 }
